4717e8049cb196cf48a63068e359d3b81cc651a1
Igor <siafin2010@gmail.com>
2020-07-20 00:10:28 +0300

    frontend draft

--- client/src/pages/authPage.jsx
+++ client/src/pages/authPage.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from '../ui/card';
 import PT from 'prop-types';
 
 import './authPage.scss'
 
 const AuthPage = () =>{
+  const [formData, setFormData] = useState({
+    email:'',
+    password:'',
+  })
+
+  const changeHandler = (e) =>{
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  
+
 
   return(
       <div className='authPage'>
 
         <Card
-          header =" Input name, email and password"
+          header ="Authentication"
         >
           <form>
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name"/>
-
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email"/>
+            <input type="email" name="email" id="email" onChange={changeHandler}/>
 
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password"/>
+            <input type="password" name="password" id="password"  onChange={changeHandler}/>
+
+            <button type="button"> Submit </button>
           </form>
         </Card>
 
