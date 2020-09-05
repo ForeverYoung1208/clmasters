5b4e2b71cd8f1c27ac5781948f611dc01cf4d66a
Igor <siafin2010@gmail.com>
2020-09-06 00:31:30 +0300

    redux implementation started...

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { GlobalDataProvider } from './context/globalDataContext';
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import * as serviceWorker from './serviceWorker';
+
+
+import App from './App';
+import rootReducer from './store/reducers/rootReducer';
+
+import './index.scss';
 
 // console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
 // console.log('process.env.REACT_APP_PRODUCTION_URL:', process.env.REACT_APP_PRODUCTION_URL);
 // console.log('process.env.REACT_APP_DEVELOPMENT_URL:', process.env.REACT_APP_DEVELOPMENT_URL);
 
+const composeEnhancers =
+  typeof window === 'object' &&  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ 
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) 
+    : compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+)
+
+
 ReactDOM.render(
   <GlobalDataProvider> 
-    <App />
+		<Provider store={store}>
+      <App />
+    </Provider>
   </GlobalDataProvider>
   ,
   document.getElementById('root')
