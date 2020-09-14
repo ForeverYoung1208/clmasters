3ea285cea5e0272ee1cf11db2c3a46c026db4a43
Igor <siafin2010@gmail.com>
2020-09-15 00:45:02 +0300

    admin page under construction

--- client/src/pages/AdminPage/AdminPage.jsx
+++ client/src/pages/AdminPage/AdminPage.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Card } from '../../components/Card/Card';
+import { CitiesBlock } from './CitiesBlock/CitiesBlock'
+import { MastersBlock } from './MastersBlock/MastersBlock';
+
 
 import './AdminPage.scss';
 
 const AdminPage = () => {
     return (
         <div className="adminPage">
-          <Card
-            header = "AdminPage Page here"
-          >  
-            AdminPage text here
+          <Card header = "Cities management" >  
+            <CitiesBlock/>
+          </Card>
+          <Card header = "Masters management" >  
+            <MastersBlock/>
           </Card>
         </div>
     );
