158863657bbc2d7196a532a55fe33e023d8cbc41
Igor <siafin2010@gmail.com>
2021-08-01 17:41:17 +0300

    masters POST endpoint tests completed

--- server/routes/__tests__/masters.routes.test.js
+++ server/routes/__tests__/masters.routes.test.js
@@ -1,6 +1,6 @@
 const app = require('../../app') // Link to server file
 const faker = require('faker')
-const { User } = require('../../models')
+const { User, City, Master } = require('../../models')
 const supertest = require('supertest')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
@@ -9,16 +9,19 @@ const request = supertest(app)
 const SALTROUNDS = parseInt(process.env.SECUR_SALTROUNDS)
 const JWTSECRET = process.env.SECUR_JWTSECRET
 
-
-//Building fake data
+// Building fake data
 const userAdmin = {
   name: 'adminUser',
   email: 'siafin2010@gmail.com',
   passwordRaw: '123456',
-  isAdmin:true,
+  isAdmin: true,
 }
 userAdmin.password = bcrypt.hashSync(userAdmin.passwordRaw, SALTROUNDS)
-userAdmin.validAccessToken = jwt.sign({ userEmail:userAdmin.email }, JWTSECRET, { expiresIn: '30d' })
+userAdmin.validAccessToken = jwt.sign(
+  { userEmail: userAdmin.email },
+  JWTSECRET,
+  { expiresIn: '30d' }
+)
 
 const userPlain = {
   name: 'plainUser',
@@ -27,10 +30,27 @@ const userPlain = {
   isAdmin: false,
 }
 userPlain.password = bcrypt.hashSync(userPlain.passwordRaw, SALTROUNDS)
-userPlain.validAccessToken = jwt.sign({ userEmail:userPlain.email }, JWTSECRET, { expiresIn: '30d' })
+userPlain.validAccessToken = jwt.sign(
+  { userEmail: userPlain.email },
+  JWTSECRET,
+  { expiresIn: '30d' }
+)
+const cityData = {
+  name: faker.random.words(1),
+  comment: faker.random.words(5),
+  isActive: true,
+}
 
-  
-  
+const masterData = {
+  name: faker.name.firstName(),
+  cityId: 'need to assignt after city creation',
+  comment: faker.random.words(5),
+  deletedAt: null,
+  rating: faker.datatype.number(5),
+  hourRate: faker.finance.amount(0, 10, 2),
+  isActive: true,
+}
+
 beforeAll(() => {
   //...
 })
@@ -38,6 +58,9 @@ beforeAll(() => {
 describe('masters POST endpoint', () => {
   beforeAll(async () => {
     await User.truncate({ cascade: true })
+    await Master.truncate({ cascade: true })
+    await City.truncate()
+    await City.create(cityData)
     await User.create(userAdmin)
     await User.create(userPlain)
   })
@@ -65,18 +88,80 @@ describe('masters POST endpoint', () => {
     expect(response.body).toMatchObject({
       error: 'not authorized',
     })
-
   })
 
-  // it('should fail when got valid token and admin account but "name" not specified', () => {
-  //     expect().
-  // })
+  it('should fail when got valid token and admin account but master\'s "name" not specified', async () => {
+    const city =await City.findOne()
+    const noNameMaster = {
+      ...masterData,
+      cityId: city.id,
+      name: null,
+    }
+    const response = await request
+      .post('/api/masters')
+      .send(noNameMaster)
+      .set('authorization', `bearer ${userAdmin.validAccessToken}`)
+      .expect(422)
 
-  // it('should fail when got valid token and admin account but "cityId" not specified', () => {
-  //     expect().
-  // })
+    expect(response.body).toMatchObject({
+      errors: [
+        {
+          location: 'body',
+          msg: 'name must be not empty!',
+          param: 'name',
+          value: null,
+        },
+      ],
+    })
+  })
 
-  // it('should create master when got valid token and admin account and valid data', () => {
-  //     expect().
-  // })
+  it('should fail when got valid token and admin account but "cityId" not specified', async () => {
+    const noNameMaster = {
+      ...masterData,
+      cityId: null,
+    }
+    const response = await request
+      .post('/api/masters')
+      .send(noNameMaster)
+      .set('authorization', `bearer ${userAdmin.validAccessToken}`)
+      .expect(422)
+
+    expect(response.body).toMatchObject({
+      errors: [
+        {
+          location: 'body',
+          msg: 'cityId must exist!',
+          param: 'cityId',
+          value: null,
+        },
+      ],
+    })
+    
+  })
+
+  it('should create master when got valid token and admin account and valid data', async () => {
+    const city =await City.findOne()
+    const noNameMaster = {
+      ...masterData,
+      cityId: city.id,
+    }
+    const response = await request
+      .post('/api/masters')
+      .send(noNameMaster)
+      .set('authorization', `bearer ${userAdmin.validAccessToken}`)
+      .expect(201)
+
+    expect(response.body).toMatchObject({
+      'cityId': expect.any(Number),
+      'cityName': cityData.name,
+      'comment': masterData.comment,
+      'deletedAt': null,
+      'hourRate': expect.any(String),
+      'id': expect.any(Number),
+      'isActive': true,
+      'name': masterData.name,
+      'rating': masterData.rating,
+    })
+
+  })
 })
