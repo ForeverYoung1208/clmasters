597617511723ce8a34c58b6af9307f275f0ef5d0
ForeverYoung1208 <siafin2010@gmail.com>
2021-09-01 18:55:00 +0300

    trying to simulate input chznge value

--- client/src/pages/MastersPage/PreorderForm/PreorderForm.test.js
+++ client/src/pages/MastersPage/PreorderForm/PreorderForm.test.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import configureStore from 'redux-mock-store'
 import PreorderForm from './PreorderForm'
 import thunk from 'redux-thunk'
 import DateFnsUtils from '@date-io/date-fns'
 import { uk } from 'date-fns/locale'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
-import pretty from 'pretty'
 import { shallow, mount, render } from 'enzyme'
+import { reducer as formReducer } from 'redux-form';
+import { compose, applyMiddleware, createStore } from 'redux'
 
-// import { render, screen, userEvent, fireEvent } from '@testing-library/react'
-// import {userEvent, fireEvent} from '@testing-library/react'
-
-import { act } from 'react-dom/test-utils'
-
-const middlewares = [thunk]
-const mockStore = configureStore(middlewares)
 const mockDispatchfn = jest.fn()
 
 const initialState = {
@@ -43,16 +36,28 @@ const initialState = {
 let container
 
 describe('testing PreorderForm', () => {
-  const props = {
-    handleSubmit: jest.fn(),
-  }
-  container = mount(
-    <MuiPickersUtilsProvider utils={DateFnsUtils} locale={uk}>
-      <Provider store={mockStore(initialState)}>
-        <PreorderForm {...props} dispatch={mockDispatchfn}></PreorderForm>
-      </Provider>
-    </MuiPickersUtilsProvider>
-  )
+  beforeEach(() => {
+    const store = createStore(
+      formReducer,
+      initialState,
+      compose(applyMiddleware(thunk))
+    )
+
+    const props = {
+      handleSubmit: jest.fn(),
+      dispatch: {mockDispatchfn}
+    }
+
+    container = mount(
+      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={uk}>
+        <Provider store={store}>
+          <PreorderForm {...props}></PreorderForm>
+        </Provider>
+      </MuiPickersUtilsProvider>
+    )
+
+  });
+  
 
   it('renders and has text "please provide some information" with class "MuiTypography-h6" ', async () => {
     expect(container.find('.MuiTypography-h6').text()).toMatch(
@@ -62,19 +67,20 @@ describe('testing PreorderForm', () => {
   
   it('requires name when it is empty', async () => {
     const inputs = container.find('input')
-    console.log(inputs)
     
-    for (let i of inputs) {
-      console.log('[i====]', i)
-    }
-    // inputs.simulate("change", {
-    //   target: { value: 1 }
-    // })
+    // for (let i of inputs) {
+    //   console.log('[i====]', i)
+    // }
     
+    inputs.first().simulate("change", {
+      target: { value: '1' }
+    })
     
-
-    
-    
+    console.log('[inputs.first]', inputs.first().debug({ verbose: true }))
+    console.log('[inputs]', inputs.debug())
   })
+
+  
+  
   
 })
