b7a34e917c51fa51d119850281c1fd2369014f35
Igor <siafin2010@gmail.com>
2021-09-04 13:14:09 +0300

    look like it is the right way

--- client/src/pages/MastersPage/PreorderForm/PreorderForm.test.js
+++ client/src/pages/MastersPage/PreorderForm/PreorderForm.test.js
@@ -6,14 +6,16 @@ import DateFnsUtils from '@date-io/date-fns'
 import { uk } from 'date-fns/locale'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
 import { shallow, mount, render } from 'enzyme'
-import { reducer as formReducer } from 'redux-form';
+import { reducer as formReducer } from 'redux-form'
 import { compose, applyMiddleware, createStore } from 'redux'
 
 import { Select } from '@material-ui/core'
 import { act } from 'react-dom/test-utils'
+import configureStore from 'redux-mock-store'
 
 const mockDispatchfn = jest.fn()
 const mockOnClickfn = jest.fn()
+const mockStore = configureStore([thunk])
 
 const initialState = {
   cities: {
@@ -33,76 +35,86 @@ const initialState = {
         type: 'small',
         repairTime: '01:00:00',
       },
+      {
+        id: 2,
+        type: 'medium',
+        repairTime: '02:00:00',
+      },
     ],
+  },
+
+  form: {
+    preorder: {
+      registeredFields: {
+        clockId: {
+          name: 'clockId',
+          type: 'Field',
+          count: 1,
+        },
+        cityId: {
+          name: 'cityId',
+          type: 'Field',
+          count: 1,
+        },
+        name: {
+          name: 'name',
+          type: 'Field',
+          count: 1,
+        },
+        email: {
+          name: 'email',
+          type: 'Field',
+          count: 1,
+        },
+        onTime: {
+          name: 'onTime',
+          type: 'Field',
+          count: 1,
+        },
+      },
+      values: {
+        onTime: '2021-09-04T09:00:00.000Z',
+        clockId: 1,
+      },
+      initial: {
+        onTime: '2021-09-04T09:00:00.000Z',
+      },
+      fields: {
+        clockId: {
+          visited: true,
+          touched: true,
+        },
+      },
+      anyTouched: true,
+    },
   },
 }
 
 let container
 
 describe('testing PreorderForm', () => {
-  beforeEach(() => {
-    const store = createStore(
-      formReducer,
-      initialState,
-      compose(applyMiddleware(thunk))
-    )
-
-    const props = {
-      handleSubmit: jest.fn(),
-      dispatch: { mockDispatchfn },
-      onClick: mockOnClickfn,
-
-    }
+  describe('testing rendering and some UI elements', () => {
+    const store = mockStore(initialState)
 
     container = mount(
       <MuiPickersUtilsProvider utils={DateFnsUtils} locale={uk}>
         <Provider store={store}>
-          <PreorderForm
-            {...props}
-            initialValues={{ clockId: 1 }}
-          />
+          <PreorderForm />
         </Provider>
       </MuiPickersUtilsProvider>
     )
 
-  });
-  
+    // console.log('[container.debug({verbose:true})]', container.debug({ignoreProps:true}))
 
-  it('renders and has text "please provide some information" with class "MuiTypography-h6" ', async () => {
-    expect(container.find('.MuiTypography-h6').text()).toMatch(
-      /please provide some information/
-    )
+    it('renders and has text "please provide some information" with class "MuiTypography-h6" ', async () => {
+      expect(container.find('.MuiTypography-h6').text()).toMatch(
+        /please provide some information/
+      )
+    })
+
+    it('renders and has fitst input (clock) has text "small" ', async () => {
+      let clockInput = container.find('#mui-component-select-clockId')
+      expect(clockInput.text()).toMatch(/small/)
+    })
   })
-  
-  it('requires name when it is empty', async () => {
-    
-    let clockInput = container.find('input').at(0)
-    act(() => {
-      clockInput.simulate('click')
-      clockInput.simulate("mousedown", { button: 0 });
-      clockInput.simulate('keypress', {keyCode:40})
-      clockInput.simulate('keypress', {keyCode:13})
-
-      //// doesn't work also      
-      // clockInput.props().onChange({ target: { value: 1 } });
-    });
-    
-    //// doesn't work also
-    // clockInput.props().onChange({ target: { value: 1 } })
-
-    ////// doesn't work also
-    // clockInput.simulate('click')
-    // clockInput.simulate("mousedown", { button: 0 });
-    // clockInput.simulate('keypress', {keyCode:40})
-    // clockInput.simulate('keypress', {keyCode:13})
-    // console.log('[clockInput]', clockInput.debug({ verbose: true }))
-    
-    
-    expect(clockInput.props().value).toBe(1)
-    
-    
-  })
-
-   
-  
 })
